94d2e51f741e63d76ac10dbf772d7dbc9c9cea55
Mattäus Sebastian Isenberg <mattaus@gmx.com>
2020-01-08 22:04:14 -0800

    Commented code and removed extraneous code

--- code401challenges/src/main/java/code401challenges/BinarySearch.java
+++ code401challenges/src/main/java/code401challenges/BinarySearch.java
@@ -4,28 +4,28 @@ import java.lang.*;
 
 public class BinarySearch {
 
-    public static void main(String[] args) {
-        int[] array2 = { 4, 8, 15, 16, 23, 42 };
-        System.out.println(binarySearch(array2, 42));
-    }
-
     public static int binarySearch(int[] array, int number) {
+        //Declare initial start, middle, and end points
         int first = 0;
         int last = array.length - 1;
         int middle = 0;
 
+        //Set initial condition to loop though all values
         while (first <= last) {
+            //Initialize value for midpoint
             middle = (first + last) / 2;
+            //Set condition and modify start point if current value is less than number
             if (array[middle] < number) {
                 first = middle + 1;
-
+                //otherwise if array value is greater adjust last endpoint
             } else if (array[middle] > number) {
                 last = middle - 1;
-
+                //When the above conditions can no longer be satisfied return the value or return -1 when while exits.
             } else {
                 return middle;
             }
         }
+        //if while loop is unable to find a match return -1 on exit
         return -1;
     }
 
