93268d1d1218ae8507ba648676cee73ef9282544
Mattäus Sebastian Isenberg <mattaus@gmx.com>
2020-01-22 11:22:48 -0800

    Final submission

--- code401challenges/src/test/java/utilities/AnimalShelterTest.java
+++ code401challenges/src/test/java/utilities/AnimalShelterTest.java
@@ -1,58 +1,65 @@
 package utilities;
 
-import org.checkerframework.checker.units.qual.K;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class AnimalShelterTest {
+public class AnimalShelterTest
+{
 
     AnimalShelter animalShelter;
     @Before
-    public void setUp(){
+    public void emptyShelter()
+    {
         animalShelter = new AnimalShelter();
     }
     @Test
-    public void enqueue() {
-        Hunde abby = new Hunde("Abby", "Belgian Malinois", "M");
-        animalShelter.enqueue(abby);
-        assertEquals("Should add Abby to the queue", abby, animalShelter.hunde_Haus.peek());
+    public void enqueue()
+    {
+        Hunde frenchy = new Hunde("French Bulldog", "Grey", "M");
+        animalShelter.enqueue(frenchy);
+        assertEquals( frenchy, animalShelter.hunde_Haus.peek());
     }
 
     @Test
-    public void dequeueDog() {
-        Hunde abby = new Hunde("Abby", "Belgian Malinois", "F");
-        animalShelter.enqueue(abby);
-        assertEquals("Should dequeue the preferred animal", abby, animalShelter.dequeue(Hunde.class));
+    public void dequeueCat()
+    {
+        Katze tino = new Katze("Manx", "Marble", "M");
+        Katze mauri = new Katze("American Short Hair", "Yellow Tabby", "M");
+        animalShelter.enqueue(tino);
+        animalShelter.enqueue(mauri);
+        assertEquals(tino, animalShelter.dequeue(Katze.class));
     }
 
     @Test
-    public void dequeueDogMultiple() {
-        Hunde abby = new Hunde("Abby", "Belgian Malinois", "M");
-        Hunde ginger = new Hunde("Ginger", "Fluff", "M");
-        animalShelter.enqueue(abby);
-        animalShelter.enqueue(ginger);
-        animalShelter.dequeue(Hunde.class);
-        assertEquals("Should dequeue the preferred animal", ginger, animalShelter.dequeue(Hunde.class));
-    }
-
-    @Test
-    public void dequeueCat() {
-        Katze peaches = new Katze("Peaches", "Tabby", "F");
-        Katze tilly = new Katze("Tilly", "Tabby", "F");
-        animalShelter.enqueue(peaches);
-        animalShelter.enqueue(tilly);
-        assertEquals("Should dequeue the preferred animal", peaches, animalShelter.dequeue(Katze.class));
-    }
-
-    @Test
-    public void dequeueCatMultiple() {
-        Katze peaches = new Katze("Peaches", "Tabby", "M");
-        Katze tilly = new Katze("Tilly", "Tabby", "F");
-        animalShelter.enqueue(peaches);
-        animalShelter.enqueue(tilly);
+    public void dequeueMultipleCats()
+    {
+        Katze tino = new Katze("Manx", "Marble", "M");
+        Katze mauri = new Katze("American Short Hair", "Yellow Tabby", "M");
+        animalShelter.enqueue(tino);
+        animalShelter.enqueue(mauri);
         animalShelter.dequeue(Katze.class);
-        assertEquals("Should dequeue the preferred animal", tilly, animalShelter.dequeue(Katze.class));
+        assertEquals(mauri, animalShelter.dequeue(Katze.class));
     }
+
+    @Test
+    public void dequeueDog()
+    {
+        Hunde frenchy = new Hunde("French Bulldog", "Grey", "M");
+        animalShelter.enqueue(frenchy);
+        assertEquals(frenchy, animalShelter.dequeue(Hunde.class));
+    }
+
+    @Test
+    public void dequeueMultipleDogs()
+    {
+        Hunde frenchy = new Hunde("French Bulldog", "Grey", "M");
+        Hunde aussie = new Hunde("Australian Shepperd", "Natural", "F");
+        animalShelter.enqueue(frenchy);
+        animalShelter.enqueue(aussie);
+        animalShelter.dequeue(Hunde.class);
+        assertEquals(aussie, animalShelter.dequeue(Hunde.class));
+    }
+
 }
