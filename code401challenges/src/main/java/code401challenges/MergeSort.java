037434e7bb3c5646bf9cb9c53fdf90d69ee1dabd
Mattäus Sebastian Isenberg <mattaus.isenberg@hotmail.com>
2020-02-11 18:32:26 -0800

    Pseudo code implemented in java successfully

---
+++ code401challenges/src/main/java/code401challenges/MergeSort.java
@@ -0,0 +1,58 @@
+package code401challenges;
+
+import java.util.Arrays;
+
+public class MergeSort
+{
+
+    public static int[] mergeSort(int[] arr)
+    {
+        int n = arr.length;
+
+        if (n > 1)
+        {
+            int mid = n / 2;
+            int[] left = Arrays.copyOfRange(arr, 0, mid);
+            int[] right = Arrays.copyOfRange(arr, mid, n);
+
+            return merge(InsertionSort.insertionSort(left), InsertionSort.insertionSort(right), arr);
+        }
+        return arr;
+    }
+
+    public static int[] merge(int[] left, int[] right, int[] arr)
+    {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length)
+        {
+            if (left[i] <= right[j])
+            {
+                arr[k] = left[i];
+                i = i + 1;
+            }
+            else
+                {
+                arr[k] = right[j];
+                j = j + 1;
+                }
+
+            k = k + 1;
+        }
+
+        if (i == left.length)
+        {
+            arr[k] = right[j];
+        } else {
+            arr[k] = left[i];
+        }
+        return arr;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] actual = {8,4,23,42,16,15};
+        System.out.println(Arrays.toString(mergeSort(actual)));
+    }
+
+}
---
+++ code401challenges/src/test/java/code401challenges/MergeSortTest.java
@@ -0,0 +1,12 @@
+package code401challenges;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MergeSortTest {
+
+    @Test
+    public void mergeSort() {
+    }
+}
