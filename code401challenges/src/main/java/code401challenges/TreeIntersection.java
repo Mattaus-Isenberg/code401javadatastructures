c2cf904f3e093b4163dfeeec898a1963ff91324f
Mattäus S. Isenberg <mattaus@gmx.com>
2020-02-20 12:31:16 -0800

    initial structure

---
+++ code401challenges/src/main/java/code401challenges/TreeIntersection.java
@@ -0,0 +1,28 @@
+package code401challenges;
+
+public class TreeIntersection
+{
+//    Code Challenge
+//    Find common values in 2 binary trees.
+//
+//    Specifications
+//    Read all of the following instructions carefully. Name things exactly as described.
+//    Do all your work in a public repository (matching the example provided by your instructor) called data-structures-and-algorithms, with a well-formatted, detailed top-level README.md
+//    Create a branch in your repository called tree-intersection
+//    On your branch, create…
+//    C#: create a method named TreeIntersection outside of Main() in your Program.cs file. Call Your newly created method in Main() once complete.
+//    JavaScript: a folder named treeIntersection which contains a file called tree-intersection.js
+//    Python: a folder named tree_intersection which contains a file called tree_intersection.py
+//    Java: a folder named TreeIntersection which contains a file called TreeIntersection.java
+//    Include any language-specific configuration files required for this challenge to become an individual component, module, library, etc.
+//    NOTE: You can find an example of this configuration for your course in your class lecture repository.
+//    Feature Tasks
+//    Write a function called tree_intersection that takes two binary tree parameters.
+//    Without utilizing any of the built-in library methods available to your language, return a set of values found in both trees.
+//    Structure and Testing
+//    Utilize the Single-responsibility principle: any methods you write should be clean, reusable, abstract component parts to the whole challenge. You will be given feedback and marked down if you attempt to define a large, complex algorithm in one function definition.
+//
+//    Write at least three test assertions for each method that you define.
+//
+//    Ensure your tests are passing before you submit your solution.
+}
