9f620f3cd9a2387b22ed11f099c618b269629bc9
Mattäus S. Isenberg <mattaus@gmx.com>
2020-02-24 13:49:42 -0800

    graph class implemented

--- code401challenges/src/main/java/code401challenges/TreeIntersection.java
+++ code401challenges/src/main/java/code401challenges/TreeIntersection.java
@@ -1,83 +1,83 @@
-package code401challenges;
-
-import stacksandqueues.Node;
-import stacksandqueues.Stack;
-import tree.BinarySearchTree;
-import tree.Tree;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-
-public class TreeIntersection
-{
-//    Code Challenge
-//    Find common values in 2 binary trees.
+//package code401challenges;
 //
-//    Specifications
-//    Read all of the following instructions carefully. Name things exactly as described.
-//    Do all your work in a public repository (matching the example provided by your instructor) called data-structures-and-algorithms, with a well-formatted, detailed top-level README.md
-//    Create a branch in your repository called tree-intersection
-//    On your branch, create…
-//    C#: create a method named TreeIntersection outside of Main() in your Program.cs file. Call Your newly created method in Main() once complete.
-//    JavaScript: a folder named treeIntersection which contains a file called tree-intersection.js
-//    Python: a folder named tree_intersection which contains a file called tree_intersection.py
-//    Java: a folder named TreeIntersection which contains a file called TreeIntersection.java
-//    Include any language-specific configuration files required for this challenge to become an individual component, module, library, etc.
-//    NOTE: You can find an example of this configuration for your course in your class lecture repository.
-//    Feature Tasks
-//    Write a function called tree_intersection that takes two binary tree parameters.
-//    Without utilizing any of the built-in library methods available to your language, return a set of values found in both trees.
-//    Structure and Testing
-//    Utilize the Single-responsibility principle: any methods you write should be clean, reusable, abstract component parts to the whole challenge. You will be given feedback and marked down if you attempt to define a large, complex algorithm in one function definition.
+//import stacksandqueues.Node;
+//import stacksandqueues.Stack;
+//import tree.BinarySearchTree;
+//import tree.Tree;
 //
-//    Write at least three test assertions for each method that you define.
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.HashSet;
 //
-//    Ensure your tests are passing before you submit your solution.
-
-    public boolean someMethod(){return true;}
-
-    public ArrayList<Integer> compare(BinarySearchTree one, BinarySearchTree two)
-    {
-        ArrayList<Integer> sameValues = new ArrayList<>();
-        Stack<Node> myStack = new Stack<>();
-        HashSet<Integer> seeNums = new HashSet<>();
-
-        myStack.push(one.getRoot_Node());
-
-        while (!myStack.isEmpty())
-        {
-            Node temp = myStack.pop();
-            seeNums.add(temp.node_Data);
-            if(temp.left != null)
-            {
-                myStack.push(temp.left);
-            }
-            if(temp.right != null)
-            {
-                myStack.push(temp.right);
-            }
-        }
-
-        myStack.push(two.getRoot_Node());
-
-        while (!myStack.isEmpty())
-        {
-            Node temp = myStack.pop();
-            if(seeNums.contains(temp.value))
-            {
-                sameValues.add(temp.value)
-            }
-            if(temp.left != null)
-            {
-                myStack.push(temp.left);
-            }
-            if(temp.right != null)
-            {
-                myStack.push(temp.right);
-            }
-        }
-
-        return  sameValues;
-    }
-}
+//public class TreeIntersection
+//{
+////    Code Challenge
+////    Find common values in 2 binary trees.
+////
+////    Specifications
+////    Read all of the following instructions carefully. Name things exactly as described.
+////    Do all your work in a public repository (matching the example provided by your instructor) called data-structures-and-algorithms, with a well-formatted, detailed top-level README.md
+////    Create a branch in your repository called tree-intersection
+////    On your branch, create…
+////    C#: create a method named TreeIntersection outside of Main() in your Program.cs file. Call Your newly created method in Main() once complete.
+////    JavaScript: a folder named treeIntersection which contains a file called tree-intersection.js
+////    Python: a folder named tree_intersection which contains a file called tree_intersection.py
+////    Java: a folder named TreeIntersection which contains a file called TreeIntersection.java
+////    Include any language-specific configuration files required for this challenge to become an individual component, module, library, etc.
+////    NOTE: You can find an example of this configuration for your course in your class lecture repository.
+////    Feature Tasks
+////    Write a function called tree_intersection that takes two binary tree parameters.
+////    Without utilizing any of the built-in library methods available to your language, return a set of values found in both trees.
+////    Structure and Testing
+////    Utilize the Single-responsibility principle: any methods you write should be clean, reusable, abstract component parts to the whole challenge. You will be given feedback and marked down if you attempt to define a large, complex algorithm in one function definition.
+////
+////    Write at least three test assertions for each method that you define.
+////
+////    Ensure your tests are passing before you submit your solution.
+//
+//    public boolean someMethod(){return true;}
+//
+//    public ArrayList<Integer> compare(BinarySearchTree one, BinarySearchTree two)
+//    {
+//        ArrayList<Integer> sameValues = new ArrayList<>();
+//        Stack<Node> myStack = new Stack<>();
+//        HashSet<Integer> seeNums = new HashSet<>();
+//
+//        myStack.push(one.getRoot_Node());
+//
+//        while (!myStack.isEmpty())
+//        {
+//            Node temp = myStack.pop();
+//            seeNums.add(temp.node_Data);
+//            if(temp.left != null)
+//            {
+//                myStack.push(temp.left);
+//            }
+//            if(temp.right != null)
+//            {
+//                myStack.push(temp.right);
+//            }
+//        }
+//
+//        myStack.push(two.getRoot_Node());
+//
+//        while (!myStack.isEmpty())
+//        {
+//            Node temp = myStack.pop();
+//            if(seeNums.contains(temp.value))
+//            {
+//                sameValues.add(temp.value)
+//            }
+//            if(temp.left != null)
+//            {
+//                myStack.push(temp.left);
+//            }
+//            if(temp.right != null)
+//            {
+//                myStack.push(temp.right);
+//            }
+//        }
+//
+//        return  sameValues;
+//    }
+//}
---
+++ code401challenges/src/main/java/code401challenges/graph/Graph.java
@@ -0,0 +1,111 @@
+package code401challenges.graph;
+
+import java.util.*;
+
+public class Graph<T>
+{
+    public static class Node<T>
+    {
+        T node_Data;
+        List<Edge<T>> neighbors;
+
+        public Node()
+        {
+            this.neighbors = new LinkedList<>();
+        }
+
+        //overloaded for node data inclusion
+        public Node(T node_Data)
+        {
+            this.node_Data = node_Data;
+            this.neighbors = new LinkedList<>();
+        }
+
+        public boolean addEdge(Node<T> neighbor, Integer weight)
+        {
+            Edge<T> edge = new Edge(neighbor, weight);
+            return neighbors.add(edge);
+        }
+
+        public T getNode_Data()
+        {
+            return this.node_Data;
+        }
+    }
+
+    public static class Edge<T>
+    {
+        public Node<T> getNeighbor() {
+            return neighbor;
+        }
+
+        public Node<T> neighbor;
+
+        public Integer getWeight() {
+            return weight;
+        }
+
+        Integer weight;
+
+        public Edge(Node<T> neighbor, Integer weight)
+        {
+            this.neighbor = neighbor;
+            this.weight = weight;
+        }
+
+    }
+
+    public Set<Node<T>> graph;
+
+    public Graph()
+    {
+        this.graph = new HashSet<>();
+    }
+
+    public boolean addNode(Node<T> node)
+    {
+        return graph.add(node);
+    }
+
+    public boolean addEdge(Node<T> node_One, Node<T> node_Two, Integer weight)
+    {
+        Node test_One = node_One;
+        Node test_Two = node_Two;
+        Set node_Check = new HashSet<>();
+        node_Check.add(test_One);
+        node_Check.add(test_Two);
+
+        if(!graph.containsAll(node_Check))
+        {
+           return false;
+        }
+        node_One.addEdge(node_Two, weight);
+        node_Two.addEdge(node_One, weight);
+        return true;
+    }
+
+    public Set getNodes()
+    {
+        return this.graph;
+    }
+
+    public List<Edge<T>> getNeighbors(Node<T> node)
+    {
+        if(graph.contains(node))
+        {
+            return node.neighbors;
+        }
+        return new LinkedList<>();
+    }
+
+    public int size()
+    {
+        return graph.size();
+    }
+
+    public boolean isEmpty()
+    {
+        return graph.isEmpty();
+    }
+
+}
